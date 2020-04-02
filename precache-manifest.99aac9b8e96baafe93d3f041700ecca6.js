243ad001584a686107a238ecc0d195afeb0f6bf9
Andrey Lomako <andrey.lomako.us@gmail.com>
2020-04-02 15:08:08 -0400

    Updates

---
+++ precache-manifest.99aac9b8e96baafe93d3f041700ecca6.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c9b5459a34d9642afff0a24c945d48ce",
+    "url": "/buttons/index.html"
+  },
+  {
+    "revision": "ea4fdf9c93f596872f30",
+    "url": "/buttons/static/css/main.9d72cbf4.chunk.css"
+  },
+  {
+    "revision": "e8baed03c1a93a336454",
+    "url": "/buttons/static/js/2.781e31de.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/buttons/static/js/2.781e31de.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ea4fdf9c93f596872f30",
+    "url": "/buttons/static/js/main.d393b282.chunk.js"
+  },
+  {
+    "revision": "099e19e013f8b101f377",
+    "url": "/buttons/static/js/runtime-main.a04c5490.js"
+  }
+]);
