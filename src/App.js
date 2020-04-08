e1dc3ccb08a4701f0063e27772cb8f11a4e34805
Andrey Lomako <andrey.lomako.us@gmail.com>
2020-04-07 20:57:42 -0400

    changed buttons for 10 100 1000 10000

--- src/App.js
+++ src/App.js
@@ -9,44 +9,53 @@ function App() {
 
     const onChangeFrom = (e) => {
         setFromDigit(+e.target.value);
-        console.log('target value', +e.target.value);
-        console.log('fromDigit', fromDigit);
         let range = [];
-        if (e.target.value > 0) {
+        if (e.target.value > 0 && e.target.value <= 100000) {
             for (let i = +e.target.value; i <= toDigit; i++) {
-                if (i !== 0) {
-                    range.push(i)
-                }
-            }
-        }
+                if (i !== 0&& i<10) {range.push(i)}
+                if (i !== 0&&i<100&&i%10===0) {range.push(i)}
+                if (i !== 0&&i<1000&&i%100===0) {range.push(i)}
+                if (i !== 0&&i<10000&&i%1000===0) {range.push(i)}
+                if (i !== 0&&i<100000&&i%10000===0) {range.push(i)}
 
+            }
+            if (toDigit!== 0&&toDigit>10&&toDigit%10!==0) {range.push(toDigit)}
+        }
         setRange(range);
     };
-    console.log(range);
-    console.log('fromDigit', fromDigit);
-
 
     const onChangeTo = (e) => {
         setToDigit(+e.target.value);
-        console.log('target value', +e.target.value);
-        console.log('toDigit', toDigit);
         let range = [];
-        if (e.target.value > 0) {
+        if (e.target.value > 0 && e.target.value <= 100000) {
             for (let i = fromDigit; i <= +e.target.value; i++) {
-                if (i !== 0) {
-                    range.push(i)
-                }
+                if (i !== 0&& i<10) {range.push(i)}
+                if (i !== 0&& i<100&&i%10===0) {range.push(i)}
+                if (i !== 0&& i<1000&&i%100===0) {range.push(i)}
+                if (i !== 0&& i<10000&&i%1000===0) {range.push(i)}
+                if (i !== 0&& i<100000&&i%10000===0) {range.push(i)}
             }
+            if (+e.target.value!== 0&&+e.target.value>10&&+e.target.value%10!==0) {range.push(+e.target.value)}
         }
+
         setRange(range);
     };
-    console.log('range', range);
-    console.log('toDigit', toDigit);
+
+    function chunk(range, chunkSize) {
+        let result = [];
+        for (let i=0;i<range.length; i+=chunkSize)
+            result.push(range.slice(i,i+chunkSize));
+        return result;
+    }
 
 
     const onButtonClickChange = (e) => {
         setNumber(number + e)
     };
+
+    const onButtonReset = () => {
+        setNumber(0)
+    }
 
     return <div className="App-content">
         <div>
@@ -56,14 +65,21 @@ function App() {
 
             <br/>
             min:
-            <input type="number" placeholder="0" min="0" title="Please enter 'from' value" onChange={onChangeFrom}/>
+            <input type="number" placeholder="0" min="0" title="Please enter minimum value" onChange={onChangeFrom}/>
             {" "}max:
-            <input type="number" placeholder="0" min="0" title="Please enter 'to' value" onChange={onChangeTo}/>
+            <input type="number" placeholder="0" min="0" title="Please enter maximum value" onChange={onChangeTo}/>
         </div>
-        <div>
+        <div className="App-buttons">
+            <div className = "left">
             {range.reverse().map(el => <button key={-el} onClick={() => onButtonClickChange(-el)}> {-el}</button>)}
-            {number}
+            </div>
+            <div className ="center">
+            {number} <br/>
+            <button onClick={() => onButtonReset()}>Reset</button>
+            </div>
+            <div className="right">
             {range.reverse().map(el => <button key={el} onClick={() => onButtonClickChange(el)}> {el}</button>)}
+            </div>
         </div>
 
 
